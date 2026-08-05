8c1db7614c721499175c0c8e425d0f3887b95b2b
githady <hardik1238@gmail.com>
2026-08-05 19:07:21 +0530

    test

--- contact-manager/vite.config.js
+++ contact-manager/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/guruji/',
+  base: '/guruji/contact-manager/',
   plugins: [react()],
 })
